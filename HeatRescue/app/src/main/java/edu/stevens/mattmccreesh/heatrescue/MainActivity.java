88a300b13637242725ae979b56677c618b9bffd5
mattmccreesh <mmccrees@stevens.edu>
2017-06-10 14:42:52 -0400

    adding location stuff

--- HeatRescue/app/src/main/java/edu/stevens/mattmccreesh/heatrescue/MainActivity.java
+++ HeatRescue/app/src/main/java/edu/stevens/mattmccreesh/heatrescue/MainActivity.java
@@ -1,22 +1,37 @@
 package edu.stevens.mattmccreesh.heatrescue;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.*;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private GoogleApiClient mGoogleApiClient;
+    private FusedLocationProviderClient mFusedLocationClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button b = (Button) findViewById(R.id.send_button);
+        b.setOnClickListener(this);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(LocationServices.API).build();
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     @Override
@@ -41,6 +56,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
-    public void onClick(View v){
+    public void onClick(View v) {
+        //Toast.makeText(this, "Test",Toast.LENGTH_LONG).show();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Success", Toast.LENGTH_LONG).show();
+            System.out.println("test");
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        //Toast.makeText(this, location.toString(), Toast.LENGTH_LONG);
+                        System.out.println(location.toString());
+                        System.out.println("test");
+                    }
+                }
+            });
+        }
+        else{
+            Toast.makeText(this, "failure", Toast.LENGTH_LONG).show();
+        }
     }
 }
