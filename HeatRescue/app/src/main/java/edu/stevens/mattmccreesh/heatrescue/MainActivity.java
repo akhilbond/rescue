dd6e25887430e76372fb6e4409f361161d5e36bf
mattmccreesh <mmccrees@stevens.edu>
2017-06-10 13:15:28 -0400

    added help button

--- HeatRescue/app/src/main/java/edu/stevens/mattmccreesh/heatrescue/MainActivity.java
+++ HeatRescue/app/src/main/java/edu/stevens/mattmccreesh/heatrescue/MainActivity.java
@@ -8,24 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Button b = (Button) findViewById(R.id.send_button);
     }
 
     @Override
@@ -48,5 +39,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v){
     }
 }
